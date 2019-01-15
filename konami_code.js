6df5e461a8e60a7b263d4799e9970a7ec1023a98
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-15 02:27:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,31 @@ const codes = [
 
 function init() {
   // your code here
+
+  // Keep track of index outside of the event handler.
+  let index = 0;
+
+  // This is the function that would be invoked by the event listener.
+  function onKeyDownHandler(e) {
+    const key = e.key;
+
+    if (key === codes[index]) {
+      index++;
+
+      if (index === codes.length) {
+        alert("Hurray!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
+
+  const input = document.body;
+  input.addEventListener('keydown', function(e) {
+    onKeyDownHandler(e);
+  });
+
+
 }
